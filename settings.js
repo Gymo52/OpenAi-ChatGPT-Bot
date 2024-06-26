8f78513d5f371939e541f70603901fa6c2fe6605
Gymo52 <166822978+Gymo52@users.noreply.github.com>
2024-06-26 06:54:55 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,9 +16,9 @@ global.ONLY_GROUPS = false // Only Groups Use
 /*
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OWNER_NAME = 'Dark Maker Ofc' // Owner Name
-global.OWNER_NUMBER = ['94715166712'] //Owner Number
-global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers Like a Sudo Numbers
+global.OWNER_NAME = 'Caesar' // Owner Name
+global.OWNER_NUMBER = ['234807811291'] //Owner Number
+global.BOT_ADMINS = ['234807811291','234807811291'] //Bot admins numbers Like a Sudo Numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
 global.IMAGE_QUALITY = 'low' // Available Quality Are => low | mediam | high
 global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴𝙽-𝙰𝙸(𝙲𝙷𝙰𝚃𝙶𝙿𝚃) 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃*'
@@ -28,7 +28,7 @@ global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴
 > Ai Bot Password Get From Watching This Video 
 */
 var SESSION_ID = ''
-global.AI_PASSWORD = 'king-nima-gpt-bot'
+global.AI_PASSWORD = Olusunle1@'
 global.SESSION_ID = process.env.SESSION_ID || SESSION_ID 
 
 /*
